daac73161a6997d65d3e507469308ead080629c4
rodionov ruslan <rodionov.R.l@gmail.com>
2023-04-28 12:05:02 +0300

    style: code style

--- src/main/java/ru/yandex/practicum/filmorate/util/AfterInternationCinemaDayValidator.java
+++ src/main/java/ru/yandex/practicum/filmorate/util/AfterInternationCinemaDayValidator.java
@@ -7,7 +7,7 @@ import java.time.LocalDate;
 
 public class AfterInternationCinemaDayValidator implements ConstraintValidator<AfterInternationCinemaDay, LocalDate> {
 
-    static final LocalDate INTERNATION_CINEMA_DAY = LocalDate.of(1895, 12, 28);
+    public static final LocalDate INTERNATION_CINEMA_DAY = LocalDate.of(1895, 12, 28);
 
     @Override
     public void initialize(AfterInternationCinemaDay constraintAnnotation) {
