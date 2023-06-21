a4ca75b23533d625ff64b2b1d83c2d151e8f395e
Иван <v.chester@yandex.ru>
2023-06-21 14:21:46 +0300

    style: fix code style 8

--- src/main/java/ru/yandex/practicum/filmorate/exception/ReviewNotFoundException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/ReviewNotFoundException.java
@@ -1,6 +1,6 @@
 package ru.yandex.practicum.filmorate.exception;
 
-public class ReviewNotFoundException extends RuntimeException{
+public class ReviewNotFoundException extends RuntimeException {
 	public ReviewNotFoundException(String message) {
 		super(message);
 	}
