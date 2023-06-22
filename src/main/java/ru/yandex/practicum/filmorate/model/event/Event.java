2f73d1f46e51d18df287153ad9de5e2d04ff6321
Серафим <seraphim.uhanov@gmail.com>
2023-06-22 12:30:15 +0300

    feat: добавить валидацию

--- src/main/java/ru/yandex/practicum/filmorate/model/event/Event.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/event/Event.java
@@ -2,15 +2,18 @@ package ru.yandex.practicum.filmorate.model.event;
 
 import lombok.Value;
 
+import javax.validation.constraints.NotBlank;
 import java.time.Instant;
 
 @Value
 public class Event {
     long eventID;
+    @NotBlank(message = "ID пользователя не может быть пустым")
     long userId;
+    @NotBlank(message = "ID сущности не может быть пустым")
     long entityId;
     EventType eventType;
     Operation operation;
     Instant timestamp = Instant.now();
-
+    
 }
